28858d287282d58f7ab06d7dd8e402a76a51fd74
Diego Saraiva <diegosaraiva@gmail.com>
2021-11-04 11:47:04 -0300

    updating

--- ldapdb/version.py
+++ ldapdb/version.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 # This software is distributed under the two-clause BSD license.
-# Copyright (c) The django-ldapdb project
+# Copyright (c) The django-ldapdb2 project
 
 import sys
 
 if sys.version_info < (3, 8):
     import pkg_resources
 
-    __version__ = pkg_resources.get_distribution('django-ldapdb').version
+    __version__ = pkg_resources.get_distribution('django-ldapdb2').version
 
 else:
     import importlib.metadata
 
-    __version__ = importlib.metadata.version('django-ldapdb')
+    __version__ = importlib.metadata.version('django-ldapdb2')
 
 VERSION = __version__
